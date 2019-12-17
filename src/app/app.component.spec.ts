9112c0274a7bdc24dd3c60030a03fe36b1bd78ce
Roland.Germ <roland.germ@nagarro.com>
2019-12-17 12:08:53 +0100

    upgraded angular

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,4 @@
-import {ComponentFixture, TestBed} from "@angular/core/testing";
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 import {AppComponent} from "./app.component";
 import {CountrySearchService} from "./country-search-service";
 
