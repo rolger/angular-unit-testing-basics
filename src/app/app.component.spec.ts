e88da365b2b25576b957759febd88e2479b0f2c4
Roland.Germ <roland.germ@nagarro.com>
2019-12-17 12:01:11 +0100

    setup working config for unit tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,8 +1,6 @@
 import {ComponentFixture, TestBed} from "@angular/core/testing";
 import {AppComponent} from "./app.component";
 import {CountrySearchService} from "./country-search-service";
-import {NO_ERRORS_SCHEMA} from "@angular/core";
-import { inject } from '@angular/core/testing';
 
 describe("Testing my-app", () => {
     let fixture: ComponentFixture<AppComponent>;
@@ -15,15 +13,14 @@ describe("Testing my-app", () => {
             declarations: [AppComponent],
             providers: [
                 {provide: CountrySearchService, useValue: mockedService}
-            ],
-            schemas: [NO_ERRORS_SCHEMA]
+            ]
         }).compileComponents();
 
         fixture = TestBed.createComponent(AppComponent);
     });
 
     it('should run', function () {
-        //expect(true).toBe(true);
+        expect(true).toBe(true);
     });
 
 
