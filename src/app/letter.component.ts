171fbe34653a641158fcda79eee5c0b3a3ad97cd
Roland.Germ <roland.germ@nagarro.com>
2020-01-03 17:15:59 +0100

    structure the code

--- src/app/letter.component.ts
+++ src/app/letter.component.ts
@@ -38,26 +38,45 @@ export class LetterComponent implements OnInit {
             });
     }
 
+
     send() {
-        if (this.content == '') {
-            this.message = { type: 'error', text: 'You must enter a text.' };
+        if (this.inputIsInvalid()) {
             return;
+        }
+
+        this.sendLetter();
+        this.clearInput();
+        this.displaySuccess();
+    }
+
+    private inputIsInvalid() {
+        if (this.content == '') {
+            this.message = {type: 'error', text: 'You must enter a text.'};
+            return true;
         }
         if (this.selectedDestination == null) {
-            this.message = { type: 'error', text: 'You must select a destination.' };
-            return;
+            this.message = {type: 'error', text: 'You must select a destination.'};
+            return true;
         }
+        return false;
+    }
 
+    private sendLetter() {
         this.deliveryService.calculateCostsAndSend(this.content, this.selectedDestination,
             this.isExpressMode ? 'EXPRESS' : 'Normal');
+    }
 
+    private clearInput() {
         this.content = '';
         this.isExpressMode = false;
         this.selectedDestination = null;
+    }
 
-        this.message = { type: 'success', text: 'Your letter has been sent.' };
-        setTimeout (() => {
+    private displaySuccess() {
+        this.message = {type: 'success', text: 'Your letter has been sent.'};
+        setTimeout(() => {
             this.message = undefined;
         }, 2000);
     }
+
 }
